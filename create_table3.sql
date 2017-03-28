6d83619a768873634889cae8089b5c6838c17955
rodrigozuniga <rodrigozuniga@gmail.com>
2017-03-27 21:34:14 -0700

    insert multiple values in table

--- create_table3.sql
+++ create_table3.sql
@@ -1,3 +1,9 @@
 create table job_git (id int, sal int);
 insert into job_git values (1,100);
 insert into job_git values (2,200);
+
+insert into job_git 
+(select 3,300 from dual
+union all
+select 4,400 from dual);
+select * from job_git;
